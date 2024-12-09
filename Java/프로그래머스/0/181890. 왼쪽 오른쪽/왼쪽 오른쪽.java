29c9767de0bfeceaf897956614c6451d111ea16a
Seon <50771727+ming-so@users.noreply.github.com>
2024-12-10 00:18:18 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.05 ms, Memory: 86.8 MB -BaekjoonHub

--- Java/프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ Java/프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -2,18 +2,16 @@ import java.util.Arrays;
 
 class Solution {
      public String[] solution(String[] str_list) {
-        String[] answer = {};
-        
+
         for (int i = 0; i < str_list.length; i++) {
-            if (str_list[i].equals("r")) {
-                if (i == str_list.length - 1) break;
+
+            if ("l".equals(str_list[i])) {
+                return Arrays.copyOfRange(str_list, 0, i);
+            } else if ("r".equals(str_list[i])) {
                 return Arrays.copyOfRange(str_list, i + 1, str_list.length);
-            } else if (str_list[i].equals("l")) {
-                if (i == 0) break;
-                return Arrays.copyOf(str_list, i);
             }
         }
-        
-        return answer;
+
+        return new String[0];
     }
 }
