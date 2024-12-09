cf7473109dae54161e2deac688c9a816e5f381f9
Seon <50771727+ming-so@users.noreply.github.com>
2024-12-10 00:23:42 +0900

    Update 왼쪽 오른쪽.java

--- Java/프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ Java/프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -2,9 +2,7 @@ import java.util.Arrays;
 
 class Solution {
      public String[] solution(String[] str_list) {
-
         for (int i = 0; i < str_list.length; i++) {
-
             if ("l".equals(str_list[i])) {
                 return Arrays.copyOfRange(str_list, 0, i);
             } else if ("r".equals(str_list[i])) {
