65e8abfac954ed571045aaa87ec63ba55b9e48a1
Seon <50771727+ming-so@users.noreply.github.com>
2024-12-25 16:33:47 +0900

    [Bronze I] Title: 평균, Time: 272 ms, Memory: 19644 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int scoreNum = scan.nextInt();
+        if(scoreNum <= 1000) {
+            int [] score = new int[scoreNum];
+            int topScore = 0;
+            for(int i = 0; i<scoreNum; i++) {
+                score[i] = scan.nextInt();
+                if(topScore<score[i]) {
+                    topScore = score[i];
+                }
+            }
+            double totalScore = 0;     
+            for(int i = 0; i<scoreNum; i++) {  
+                totalScore += (double)score[i]/topScore*100;     
+            }
+            System.out.println((double)totalScore/scoreNum);            
+        } 
+        scan.close();
+    }
+}
